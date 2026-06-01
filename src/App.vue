755701f13a6968716b705b5e716aaa57b6c926e1
Zachary Klosko <zklosko@hopecentral.com>
2026-06-01 18:53:53 -0400

    feat: adding half and quarter values

--- src/App.vue
+++ src/App.vue
@@ -34,26 +34,46 @@ watch(
     </section>
     <div class="box">
       <div class="block">
-        <div class="field">
-          <div class="label">Target BPM</div>
-          <div class="control">
+        <div class="label">Target BPM</div>
+        <div class="field has-addons">
+          <div class="control is-expanded">
             <input class="input" type="text" placeholder="BPM" v-model="bpmField" />
+          </div>
+          <div class="control">
+            <button class="button">
+              <span class="icon"><i class="fa-solid fa-xmark"></i></span
+              ><span><i class="fa-solid fa-2"></i></span>
+            </button>
+          </div>
+          <div class="control">
+            <button class="button is-info">
+              <span class="icon"><i class="fa-solid fa-divide"></i></span
+              ><span><i class="fa-solid fa-2"></i></span>
+            </button>
           </div>
         </div>
       </div>
       <div class="block">
         <div class="columns">
-          <div class="column">
-            Speed value:
-            <p class="has-text-weight-extrabold">{{ bpmCalc.val }}</p>
+          <div class="column has-text-centered">
+            <div class="block"><p class="is-size-6">Speed value:</p></div>
+            <div class="block">
+              <p class="has-text-weight-extrabold">Whole: {{ bpmCalc.val }}</p>
+              <p class="has-text-weight-medium">Half: {{ bpmCalc.val / 2 }}</p>
+              <p class="has-text-weight-medium">Quarter: {{ bpmCalc.val / 4 }}</p>
+            </div>
           </div>
-          <div class="column">
-            Actual BPM:
-            <p class="has-text-weight-semibold">{{ bpmCalc.actualBpm }}</p>
+          <div class="column has-text-centered">
+            <div class="block"><p class="is-size-6">Actual BPM:</p></div>
+            <div class="block">
+              <p class="has-text-weight-semibold">{{ bpmCalc.actualBpm }}</p>
+            </div>
           </div>
-          <div class="column">
-            BPM error:
-            <p class="has-text-weight-semibold" :class="errClass">{{ bpmCalc.err }}</p>
+          <div class="column has-text-centered">
+            <div class="block"><p class="is-size-6">BPM error:</p></div>
+            <div class="block">
+              <p class="has-text-weight-semibold" :class="errClass">{{ bpmCalc.err }}</p>
+            </div>
           </div>
         </div>
       </div>
